6072dd08f33858357f3a7058ed48884cb87a606f
Ausmarton Zarino Fernandes <ausmarton@gmail.com>
2012-07-16 12:23:55 +0530

    Added select all checkbox. Delete removes documents from the table

--- couch.bulk.delete.js
+++ couch.bulk.delete.js
@@ -2,33 +2,52 @@ function delete_doc(doc_url) {
 	$.ajax({
 		url: doc_url,
 		type: 'DELETE',
-		success: function() { console.log('Deleted: ' + doc_url); },
+		success: function() { 
+			console.log('Deleted: ' + doc_url);
+			$('#' + doc_url.replace(/\/|\?|\=/g,'')).remove(); 
+		},
 		error: function() { console.log('Error: ' + doc_url); }
 	});
 }
 
 $(document).ready(function() {
-	$('ul#toolbar').append('<li><button id="bulk_delete_enable">Enable Bulk Delete</button></li>');
-	$('#bulk_delete_enable').click(function() {
-		$(this).text('Delete Documents');
-		$(this).attr('id','bulk_delete');
-		$(this).unbind('click');
-		enable_bulk_delete();
-	});
-});
-
-function enable_bulk_delete() {
-	$('tbody.content tr').each(function() {
-		doc = $(this).children('td.key').children('a').attr('href').split('?');
-		doc_id = '';
-		if(doc.length == 2)
-			doc_id = doc[1];
-		rev=$(this).children('td.value').find('code.string').text().split('"');
-		$(this).append($('<td><input class="doc_bulk_select" type="checkbox" doc_link="/'+ doc_id +'?rev=' + rev[1] + '" ></input></td>')); 
+	$('ul#toolbar').append('<li><button id="bulk_delete">Delete Documents</button></li>');
+	$('thead tr').each(function() {
+		$(this).append('<th><input id="select_all" type="checkbox"></input></th>');
+		$('#select_all').change(function() {
+			all=$('input.doc_bulk_select').length;
+			if(all == 0)
+				create_checkboxes();
+			checked=$('input.doc_bulk_select:checked').length;
+			if(all == checked && $(this).attr('checked') == false) {
+				$('input.doc_bulk_select:checked').each(function() {
+                                        $(this).attr('checked',false);
+                                });
+			} else {	
+				$('input.doc_bulk_select').not(':checked').each(function() {
+					$(this).attr('checked',true);
+				});
+			}
+		});
 	});
 	$('#bulk_delete').click(function() {
 		$('input.doc_bulk_select:checked').each(function(){
 			delete_doc($(this).attr('doc_link'));
 		});
 	});
+});
+
+function create_checkboxes() {
+        $('tbody.content tr').each(function() {
+		if($(this).children('td').length == 3)
+			return;
+                doc = $(this).children('td.key').children('a').attr('href').split('?');
+                doc_id = '';
+                if(doc.length == 2)
+                        doc_id = doc[1];
+                rev=$(this).children('td.value').find('code.string').text().split('"');
+                doc_url = '/'+ doc_id +'?rev=' + rev[1];
+		$(this).append($('<td><input class="doc_bulk_select" type="checkbox" doc_link="' + doc_url + '" ></input></td>'));
+		$(this).attr('id',doc_url.replace(/\/|\?|\=/g,''));
+        });
 }
